d5274d7aaf166d5ccd5cc10c5bf6f5f67af49457
cygami <cygami-vr@gmail.com>
2026-05-02 11:35:50 -0500

    Sliders now show the current value

--- src/components/changer/Slider.vue
+++ src/components/changer/Slider.vue
@@ -1,22 +1,46 @@
 <script setup lang="ts">
 import ThemedSlider from '@/components/theme/ThemedSlider.vue'
 import LockButton from '@/components/changer/LockButton.vue'
+import { reactive, watch } from 'vue'
 
 const props = defineProps(['param'])
+const state = reactive({
+  current: 0,
+})
 const emit = defineEmits(['change', 'lock'])
+
+watch(
+  () => props.param.value,
+  (val) => {
+    if (typeof val == 'number') {
+      state.current = Number(val.toFixed(2))
+    } else {
+      state.current = 0
+    }
+  },
+  { immediate: true }
+)
 
 const min = parseFloat(props.param.minValue) || 0
 const max = parseFloat(props.param.maxValue) || 1
 const range = max - min
 
+function onInput(evt: Event) {
+  if (evt && evt.target && evt.target instanceof HTMLInputElement) {
+    state.current = getValue(evt.target)
+  }
+}
+
 function onChange(evt: Event) {
   if (evt && evt.target && evt.target instanceof HTMLInputElement) {
-    emit('change', props.param.name, getValue(evt.target))
+    state.current = getValue(evt.target)
+    emit('change', props.param.name, state.current)
   }
 }
 
 function getValue(input: HTMLInputElement) {
-  return (parseFloat(input.value) / 100) * range + min
+  const value = (parseFloat(input.value) / 100) * range + min
+  return Number(value.toFixed(2))
 }
 </script>
 
@@ -29,11 +53,13 @@ function getValue(input: HTMLInputElement) {
   <div class="text-center mb-1" v-if="param.image">
     <img :src="param.image" class="rounded-3" />
   </div>
+  <div class="text-center text-body mb-1">{{ state.current }}</div>
   <ThemedSlider
     defaultClass="w-75"
     :disabled="param.locked"
     :value="((param.value - min) / range) * 100"
     :name="param.name"
+    @input="onInput"
     @change="onChange"
   />
 </template>
